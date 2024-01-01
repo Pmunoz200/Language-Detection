aa34ae357d374d74ad03b93fdf47873f0de35716
Pablo Munoz s310308 <s310308@studenti.polito.it>
2024-01-01 20:02:56 +0100

    Finished SVM code

--- SVM.py
+++ SVM.py
@@ -41,32 +41,120 @@ def svm_train(attributes, labels, C, model= "svm", pi=[0.5], k = 5, pca = 0):
     title += f"with kernel model {model}." if model != "svm" else "."
     title = title + f"PCA: {pca}" if pca else title
     for c in C:
-        for p in pi:
-            [_, _, accuracy, DCFnorm, minDCF] = ML.k_fold(
+        [_, _, accuracy, minDCF] = ML.k_fold(
                 k,
                 attributes,
                 labels,
                 priorProb,
                 model=model,
                 C=c,
-                pi=p,
+                pi=pi,
                 PCA_m=pca
             )
+        for p in pi:
             if p== 0.1:
-                minDCFvalues1.append(minDCF)
+                minDCFvalues1.append(minDCF[p])
                 print(minDCFvalues1)
             if p== 0.2:
-                minDCFvalues2.append(minDCF)
+                minDCFvalues2.append(minDCF[p])
             if p== 0.5:
                 print(minDCFvalues1[-1])
-                val = (minDCF + minDCFvalues1[-1])/2
+                val = (minDCF[p] + minDCFvalues1[-1])/2
                 minDCFvalues5.append(val)
     
     plt.figure(figsize=(10, 6))
     plt.semilogx(C, minDCFvalues1, label=f'π= 0.1')
     plt.semilogx(C, minDCFvalues2, label=f'π= 0.2')
     plt.semilogx(C, minDCFvalues5, label=f'π= avg(0.1, 0.5)')
-    plt.xlabel('lambda')
+    plt.xlabel('Constant C')
+    plt.ylabel('minDCF')
+    plt.title(title)
+    plt.legend()
+    plt.show()
+
+def svm_gamma_train(attributes, labels, C, model= "svm", pi=[0.1,0.2, 0.5], k = 5, pca = 0, gamma = 1):
+    minDCFvalues1=[]
+    minDCFvalues2=[]
+    minDCFvalues5=[]
+    title = "Support Vector Machine"
+    title += f"with kernel model {model}." if model != "svm" else "."
+    title = title + f"PCA: {pca}" if pca else title
+    for g in gamma:
+        [_, _, accuracy, minDCF] = ML.k_fold(
+                k,
+                attributes,
+                labels,
+                priorProb,
+                model=model,
+                C=C,
+                pi=pi,
+                PCA_m=pca,
+                gamma=g
+            )
+        for p in pi:
+            if p== 0.1:
+                minDCFvalues1.append(minDCF[p])
+                print(minDCFvalues1)
+            if p== 0.2:
+                minDCFvalues2.append(minDCF[p])
+            if p== 0.5:
+                print(minDCFvalues1[-1])
+                val = (minDCF[p] + minDCFvalues1[-1])/2
+                minDCFvalues5.append(val)
+    
+    plt.figure(figsize=(10, 6))
+    plt.semilogx(gamma, minDCFvalues1, label=f'π= 0.1')
+    plt.semilogx(gamma, minDCFvalues2, label=f'π= 0.2')
+    plt.semilogx(gamma, minDCFvalues5, label=f'π= avg(0.1, 0.5)')
+    plt.xlabel('gamma')
+    plt.ylabel('minDCF')
+    plt.title(title)
+    plt.legend()
+    plt.show()
+
+
+def svm_b_c_train(attributes, labels, C, dim, c, model= "polynomial", pi=[0.1, 0.5], k = 5, pca = 0):
+    minDCFvalues1=0
+    graph_vals = []
+    title = "Support Vector Machine"
+    title += f"with kernel model {model}." if model != "svm" else "."
+    title = title + f"PCA: {pca}" if pca else title
+    lists = 0
+    for constrain in C:
+        cont = 0
+        for b in dim:
+            for const in c:
+                if lists < 4:
+                    graph_vals.append([])
+                    lists += 1
+                [_, _, accuracy, minDCF] = ML.k_fold(
+                    k,
+                    attributes,
+                    labels,
+                    priorProb,
+                    model=model,
+                    C=constrain,
+                    pi=pi,
+                    PCA_m=pca,
+                    dim=b,
+                    c=const
+                )
+                for p in pi:
+                    if p== 0.1:
+                        minDCFvalues1 = minDCF[p]
+                    if p== 0.5:
+                        print(minDCFvalues1)
+                        val = (minDCF[p] + minDCFvalues1)/2
+                        graph_vals[cont].append(val)
+                        cont += 1
+    
+    plt.figure(figsize=(10, 6))
+    cont = 0
+    for b in range(len(dim)):
+        for i in range(len(c)):
+            plt.semilogx(C, graph_vals[cont], label=f'dim = {dim[b]} c= {c[i]}')
+            cont += 1
+    plt.xlabel('C')
     plt.ylabel('minDCF')
     plt.title(title)
     plt.legend()
@@ -74,7 +162,7 @@ def svm_train(attributes, labels, C, model= "svm", pi=[0.5], k = 5, pca = 0):
 
 if __name__ == "__main__":
     ## Path for debbug ##
-    # path = "PythonCode/Data/Train.txt"
+    path = "PythonCode/Data/Train.txt"
 
     ## Path for normal running ##
     path = "Data/Train.txt"
@@ -86,22 +174,16 @@ if __name__ == "__main__":
     Cfn = 1
     Cfp = 1
     initial_C = np.logspace(-6, 6, 10)
-    initial_K = 1
+    gamma = np.logspace(-3,3,10)
     k = 5
-    headers = [
-    "Model",
-    "π=0.5 DCF/minDCF",
-    "π=0.3 DCF/minDCF"
-    "π=0.7 DCF/minDCF",
-    ]
 
-    ### -------full dataset ----- ###
+    # ### -------full dataset ----- ###
     svm_train(full_train_att, full_train_label, initial_C, pi=pi)
     svm_train(full_train_att, full_train_label, initial_C, pi=pi, model="polynomial")
     svm_train(full_train_att, full_train_label, initial_C, pi=pi, model="radial")
 
 
-    ### -------z-score----------- ###
+    # ### -------z-score----------- ###
     standard_deviation = np.std(full_train_att)
     z_data = ML.center_data(full_train_att) / standard_deviation
 
@@ -109,62 +191,58 @@ if __name__ == "__main__":
     svm_train(z_data, full_train_label, initial_C, pi=pi, model="polynomial")
     svm_train(z_data, full_train_label, initial_C, pi=pi, model="radial")
     
-    # ### --------z-score----------###
-    # standard_deviation = np.std(full_train_att)
-    # z_data = ML.center_data(full_train_att) / standard_deviation
-    # section_size = int(full_train_att.shape[1] / k)
-    # low = 0
-    # all_values = np.c_[z_data.T, full_train_label]
-    # all_values = np.random.permutation(all_values)
-    # attributes = all_values[:, 0:12].T
-    # labels = all_values[:, -1].astype("int32")
-    # high = section_size
-    # if high > attributes.shape[1]:
-    #     high = attributes.shape
-    # test_att = attributes[:, low:high]
-    # test_labels = labels[low:high]
-    # train_att = attributes[:, :low]
-    # train_label = labels[:low]
-    # train_att = np.hstack((train_att, attributes[:, high:]))
-    # train_label = np.hstack((train_label, labels[high:]))
+    ### -------Gamma search----------- ###
+    svm_gamma_train(full_train_att, full_train_label, 0.1, model="radial", gamma=gamma)
+    svm_gamma_train(z_data, full_train_label, 0.1, model="radial", gamma=gamma)
 
-    # minDCFvalues5Z = []
-    # minDCFvalues3Z = []
-    # minDCFvalues7Z = []
+    ## -------b and c search----------- ###
+    full_dataset_C = np.logspace(-5, -1, 8)
+    svm_b_c_train(full_train_att, full_train_label, full_dataset_C, [2, 3], [0,1], model="polynomial")
+    z_dataset_C = np.logspace(-2, 1, 8)
+    svm_b_c_train(z_data, full_train_label, z_dataset_C, [2, 3], [0,1], model="polynomial")
 
-    # for p in pi:
-    #     for C in initial_C:
-    #         contrain = C
 
-    #         k = initial_K * np.power(10, 0)
-    #         [SPost, Predictions, accuracy] = ML.svm(
-    #             train_att, train_label, test_att, test_labels, contrain, K=k,
-    #         )
-    #         confusion_matrix = ML.ConfMat(Predictions, test_labels)
-    #         DCF, DCFnorm = ML.Bayes_risk(confusion_matrix, p, Cfn, Cfp)
-    #         (minDCF, FPRlist, FNRlist, _) = ML.minCostBayes(
-    #             SPost, test_labels, p, Cfn, Cfp
-    #         )
-    #         if p == 0.5:
-    #             minDCFvalues5Z.append(minDCF)
-    #         if p == 0.3:
-    #             minDCFvalues3Z.append(minDCF)
-    #         if p == 0.7:
-    #             minDCFvalues7Z.append(minDCF)
-    # tableFullZ=[]
-    # cont=0
-    # for p in pi:
-    #     tableFullZ.append([f"SVM pi= {p}"])
-    #     for x in pi:
-    #         contrain = searchC
-    #         k = initial_K * np.power(10, 0)
-    #         [SPost, Predictions, accuracy] = ML.svm(
-    #             train_att, train_label, test_att, test_labels, contrain, K=k,pit=p
-    #         )
-    #         confusion_matrix = ML.ConfMat(Predictions, test_labels)
-    #         DCF, DCFnorm = ML.Bayes_risk(confusion_matrix, x, Cfn, Cfp)
-    #         (minDCF, FPRlist, FNRlist, _) = ML.minCostBayes(
-    #             SPost, test_labels, x, Cfn, Cfp
-    #         )
-    #         tableFullZ[cont].append([DCFnorm,minDCF])
-    #     cont+=1
+
+    ### -------Searching for the final table of values----------- ###
+    c_vals = [0.1, 0.1, 0.0001]
+    headers = [
+        "Radial SVM",
+        "Polynomial SVM z_norm",
+        "Polynomial SVM",
+    ]
+    tableKFold = []
+    pit = [0.1, 0.2, 0.5]
+    gamma = 0.01
+    cont = 0
+    for x in range(len(headers)):
+        tableKFold.append([f"SVM {x}"])
+        if headers[x].split(" ")[-1] == "z_norm":
+            [_, _, accuracy, minDCF] = ML.k_fold(
+                    k,
+                    z_data,
+                    full_train_label,
+                    priorProb,
+                    headers[x].split(" ")[0].lower(),
+                    pi=pi,
+                    C=c_vals[x]
+                )
+        else:
+            [_, _, accuracy, minDCF] = ML.k_fold(
+                    k,
+                    full_train_att,
+                    full_train_label,
+                    priorProb,
+                    headers[x].split(" ")[0].lower(),
+                    pi=pi,
+                    C=c_vals[x],
+                    gamma=gamma
+                )
+        for p in pi:
+            if p == 0.5:
+                print(minDCF)
+                minDCF[p] = (minDCF[0.1] + minDCF[p]) / 2
+                print(minDCF)
+            tableKFold[cont].append([minDCF[p]])
+        cont += 1
+   
+    print(tabulate(tableKFold, headers))
