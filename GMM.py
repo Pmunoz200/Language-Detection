c4fcdc0f0adbf1b6d4ac8273a8ca3353e3107316
Pablo Munoz s310308 <s310308@studenti.polito.it>
2024-01-20 12:51:18 +0100

    Finished GMM model evaluation

--- GMM.py
+++ GMM.py
@@ -38,40 +38,42 @@ def call_GMM(attributes, labels, prior_probability, models, k_value=5, m = 0, pi
     tableGMM = []
     total_iter = len(models)*len(pi)
     perc = 0
-
+    cont = 0
+    headers = []
+    headers.append([f"MinDCF for {pi} prior probability"])
     ### ------------- K-FOLD CROSS-VALIDATION AND PCA ---------------------- ####
-    tableGMM.append(["Full"])
     result_minDCF = []
     c2 = 1
     list_minDCF = []
-    list_DCF = []
     for model in models:
+        tableGMM.append([model])
         ref = model.split(":")
         mod = ref[0]
         constrains = eval(ref[1])
-        tableGMM[0].append([])
+        tableGMM[cont].append([])
+        if not m:
+            [S, _, _, minDCF] = ML.k_fold(
+            k_value, attributes, labels, prior_probability, model=mod, niter=constrains[1],alpha=constrains[0], psi=constrains[2], pi=pi
+            )
+        else:
+            [S, _, _, minDCF] = ML.k_fold(
+                k_value, attributes, labels, prior_probability, model=mod, niter=constrains[1],alpha=constrains[0], psi=constrains[2], PCA_m=m, pi=pi
+                )
+            
         for p in pi:
-            if not m:
-                [S, pred, _, DCFnorm, minDCF] = ML.k_fold(
-                k_value, attributes, labels, prior_probability, model=mod, niter=constrains[1],alpha=constrains[0], psi=constrains[2], pi=p
-                )
-            else:
-                [S, pred, _, DCFnorm, minDCF] = ML.k_fold(
-                k_value, attributes, labels, prior_probability, model=mod, niter=constrains[1],alpha=constrains[0], psi=constrains[2], PCA_m=m, pi=p
-                )
-            tableGMM[0][c2].append(minDCF)
+            if p == 0.5:
+                minDCF[p] = (minDCF[0.1] + minDCF[p]) / 2
+                result_minDCF.append(minDCF[p])
+            tableGMM[cont][c2].append([minDCF[p]])
             perc += 1
             list_minDCF.append(minDCF)
-            list_DCF.append(DCFnorm)
-            print(f"{round(perc * 100 / total_iter, 2)}%")
-        result_minDCF.append(minDCF)
-        c2 += 1
-    cont = 1
-    print("minDCF table")
-    print(tabulate(tableGMM, headers=models[0:3]))
+            print(f"{round(perc * 100 / total_iter, 2)}%", end=" ")
+        cont += 1
+    print("\tminDCF table")
+    print(tabulate(tableGMM, headers=headers))
     return result_minDCF, S
 
-def graph_data(raw_results, z_results, model, pca = 0):
+def graph_data(raw_results, z_results, model, pca = 0, p_labels = 1):
     attribute = {
     "Raw": raw_results,
     "Z-Score": z_results
@@ -91,7 +93,8 @@ def graph_data(raw_results, z_results, model, pca = 0):
     for attribute, measurement in attribute.items():
         offset = width * multiplier
         rects = ax.bar(x + offset, measurement, width, label=attribute)
-        ax.bar_label(rects, padding=3)
+        if p_labels:
+            ax.bar_label(rects, padding=3) 
         multiplier += 1
 
     # Add some text for labels, title and custom x-axis tick labels, etc.
@@ -108,6 +111,9 @@ def graph_data(raw_results, z_results, model, pca = 0):
 
 if __name__ == "__main__":
     
+    ## Path for debbug ##
+    path = "PythonCode/Data/Train.txt"
+    ## Path for normal running ##
     path = "data/Train.txt"
     [full_train_att, full_train_label] = load(path)
     print(os.getcwd())
@@ -122,22 +128,36 @@ if __name__ == "__main__":
     # headers = [
     # "GMM:[0.1, 2, 0.01]",
     # ]
+    model = "GMM"
+    labels = 1
+    pca = 0
     headers = [
-    "GMM:[0.1, 0, 0.01]",
-    "GMM:[0.1, 1, 0.01]",
-    "GMM:[0.1, 2, 0.01]",
-    "GMM:[0.1, 3, 0.01]",
-    "GMM:[0.1, 4, 0.01]",
-    "GMM:[0.1, 5, 0.01]",
+    f"{model}:[0.1, 0, 0.01]",
+    f"{model}:[0.1, 1, 0.01]",
+    f"{model}:[0.1, 2, 0.01]",
+    f"{model}:[0.1, 3, 0.01]",
+    f"{model}:[0.1, 4, 0.01]",
+    f"{model}:[0.1, 5, 0.01]",
     ]
-    pi = [0.1, 0.2, 0.5]
+    # headers = ["GMM:[0.1, 2, 0.01]","Tied:[0.1, 5, 0.01]"]
+    pi = [0.1, 0.2,  0.5]
     standard_deviation = np.std(full_train_att)
     z_data = ML.center_data(full_train_att) / standard_deviation
     
-    print("Full data")
-    [raw_values, S_raw] = call_GMM(full_train_att, full_train_label, priorProb, pi=pi, models=headers)
+    print("Raw data")
+    [raw_values, S_raw] = call_GMM(full_train_att, full_train_label, priorProb, pi=pi, models=headers, m=pca)
+    print("Z-norm data")
+    [z_values, S_z] = call_GMM(z_data, full_train_label, priorProb, pi=pi, models=headers, m=pca)
+    headers = ["Tied:[0.1, 5, 0.01]"]
+    pca = 5
+    print("Raw data")
+    [raw_values, S_raw] = call_GMM(full_train_att, full_train_label, priorProb, pi=pi, models=headers, m=pca)
+    print("Z-norm data")
+    [z_values, S_z] = call_GMM(z_data, full_train_label, priorProb, pi=pi, models=headers, m=pca)
+    graph_data(raw_values, z_values, f"{model} with PCA" if pca else f"model", p_labels=labels)
     np.save("./Score_GMM", S_raw)
-    print(raw_values)
+
+    # Bayes Error Plot
     pred_raw = ML.calculate_model(S_raw, full_train_att, "Generative", priorProb)
     conf_RAW = ML.ConfMat(pred_raw, full_train_label)
     ML.BayesErrorPlot(S_raw, full_train_label,conf_RAW,Cfn, Cfp)
