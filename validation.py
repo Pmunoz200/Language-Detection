a46a27dd929be396f8d4763344c9149467028e54
Pablo Munoz s310308 <s310308@studenti.polito.it>
2024-02-11 14:05:12 +0100

    Almost finished validation, missing fixing RSVM and GMM scores

--- validation.py
+++ validation.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import pandas as pd
 import scipy
@@ -6,6 +7,7 @@ from tabulate import tabulate
 import sys
 import matplotlib.pyplot as plt
 from MLandPattern import MLandPattern as ML
+from scipy.optimize import fmin_l_bfgs_b
 
 tableTrain = []
 tableTest = []
@@ -47,8 +49,9 @@ def split_db(D, L, fraction, seed=0):
     return (DTR, LTR), (DTE, LTE)
 
 def calculate_logreg(test_points, w, b, quadratic = 0):
-    xt_2 = np.dot(test_points.T, test_points).diagonal().reshape((1, -1)) if quadratic else np.array([])
-    test_points = np.vstack((xt_2, test_points))
+    if quadratic:
+        xt_2 = np.dot(test_points.T, test_points).diagonal().reshape((1, -1))
+        test_points = np.vstack((xt_2, test_points))
     S = np.dot(w.T, test_points) + b
     return S
 
@@ -129,6 +132,59 @@ def calculate_model(
     error = np.sum(error) / test_labels.shape[0]
     return predictions, (1 - error),  minDCF_dic
 
+def logreg_obj(v, DTR, LTR, l, n):
+    # compute J(w,b) = l/2*||w||^2 + 1/n*sum(log(1+e^(-zi*(wt*xi+b))))
+    # given zi = 1 if ci=1 and =0 if ci=0
+    
+    w, b = v[0:-1], v[-1]
+    J = l/2 * np.dot(w, w.T)                 #numpy.linalg.norm(w)
+    for i in range(n):
+        zi = 1 if LTR[i]==1 else -1
+        if DTR.ndim > 1:
+            J += np.logaddexp(0, -zi * (np.dot(w.T, DTR[:,i]) + b)) / n
+        else:
+            J += np.logaddexp(0, -zi * (np.dot(w.T, DTR[i]) + b)) / n
+    return J
+
+def linear_regression_2C(feat, labels, l):
+    x0 = np.zeros(feat.shape[0] + 1)
+    
+    opt,_1,_2 = fmin_l_bfgs_b(logreg_obj, x0, approx_grad=True, args=(feat, labels, l, labels.shape[0]))
+    wopt, bopt = opt[0:-1], opt[-1]
+    
+    return wopt, bopt
+
+def score_calibration(K, DTR, LTR, l, LOO=False, testing = False):  
+    """K is n of folds, lambda and model must be a function that returns an array of predictions"""
+    #given DTR, LTR and K compute K-folds cross validation (Leave One Out with K = N)
+    #give a model function that accepts DTR, LTR, DTV and gives out some predictions to be confronted with LTV
+    if DTR.ndim == 1:
+        DTR = ML.vrow(DTR)
+    N = DTR.shape[1]
+    if(LOO):
+        K = N
+    seglen = N//K
+    #costs = []
+    alls = []
+    if testing:
+        wopt, bopt = 0, 0
+    for i in range(K):
+        mask = [True] * N
+        mask[i*seglen : (i+1)*seglen] = [False] * seglen
+        notmask = [not x for x in mask]
+        DTRr = DTR[:,mask]
+        LTRr = LTR[mask]
+        DTV = DTR[:,notmask]
+        #LTV = LTR[notmask]
+        #LTV = LTR[notmask]        
+        wopt, bopt = linear_regression_2C(DTRr, LTRr, l)
+        scores = np.dot(ML.vrow(wopt), DTV) + bopt
+        if testing:
+            wopt += wopt
+            bopt += bopt
+        alls += scores.ravel().tolist()
+    return alls, wopt, bopt if testing else alls
+
 
 if __name__ == "__main__":
 
@@ -151,17 +207,24 @@ if __name__ == "__main__":
     ## Hyper-parameters k-fold
     k = 5
     pi = [0.1, 0.5]
+
+    ## Data sets 
     pathTrain = os.path.abspath("Data/Train.txt")
     [full_train_att, full_train_label] = load(pathTrain)
+    standard_deviation = np.std(full_train_att)
+    z_data = ML.center_data(full_train_att) / standard_deviation
 
     pathTest = os.path.abspath("Data/Test.txt")
     [full_test_att, full_test_labels] = load(pathTest)
+    standar_test = np.std(full_test_att)
+    z_test = ML.center_data(full_test_att) / standar_test
 
     priorProb = ML.vcol(np.ones(2) * 0.5)
     tableTest.append([])
     tableTrain.append([])
 
-    # ## Quadratic Regression
+    ## Quadratic Regression
+    # print("Training QR...")
     # [_, _, minDCF, w, b] = ML.k_fold(
     #     k,
     #     full_train_att,
@@ -175,7 +238,8 @@ if __name__ == "__main__":
     #     final=1,
     # )
 
-    # ## Test Quadratic Regression
+    # # ## Test Quadratic Regression
+    # print("Testing QR...")
     # scr_quadra = calculate_logreg(full_test_att, w, b, 1)
     # for p in pi:
     #     (minDCF, _, _, _) = ML.minCostBayes(scr_quadra, full_test_labels, p, Cfn, Cfp)
@@ -184,51 +248,73 @@ if __name__ == "__main__":
 
     # ML.BayesErrorPlot(scr_quadra, full_test_labels, Cfn, Cfp, "Quadratic Regression")
 
-    # ## Calculate Radial SVM
-    # [_,  minDCF, attribute_tra_rbm, labels_tra_rbm, x] = ML.k_fold(
-    #     k,
-    #     full_train_att,
-    #     full_train_label,
-    #     priorProb,
-    #     "radial",
-    #     pi=[0.1, 0.5],
-    #     C=C,
-    #     gamma=gamma,
-    #     final=1,
-    # )
-
-    # ## Test Radial SVM
-    # zi = 2 * labels_tra_rbm - 1
-    # scr_poly = calculate_svm(x, zi, attribute_tra_rbm, full_test_att, {"model":"radial", "gamma":gamma, "epsilon":0.1})
-
-    # for p in pi:
-    #     (minDCF, _, _, _) = ML.minCostBayes(scr_poly, full_test_labels, p, Cfn, Cfp)
-    #     minDCffull_list.append(minDCF)
-    # print("Radial SVM MinDCF: ", minDCffull_list[-2:], "Mean: ", np.mean(minDCffull_list[-2:]))
-    # ML.BayesErrorPlot(scr_poly, full_test_labels, Cfn, Cfp, "Radial SVM")
-
-    # Calculate Polynomial SVM
-    [_,  minDCF, attribute_tra_poly, labels_tra_poly, x] = ML.k_fold(
+    ## Calculate Radial SVM
+    print("Training RSVM...")
+    [SRad,  _, attribute_tra_rbm, labels_tra_rbm, x] = ML.k_fold(
         k,
         full_train_att,
         full_train_label,
         priorProb,
-        "polynomial",
+        "radial",
         pi=[0.1, 0.5],
         C=C,
-        dim=dim,
-        c=c,
+        gamma=gamma,
         final=1,
     )
 
-    ## Test Polynomial SVM
-    zi = 2 * labels_tra_poly - 1
-    scr_poly = calculate_svm(x, zi, attribute_tra_poly, full_test_att, {"model":"polynomial", "dim":dim, "c":c, "epsilon":0.1})
-    print("Polynomial SVM MinDCF: ", minDCffull_list[-2:], "Mean: ", np.mean(minDCffull_list[-2:]))
-    ML.BayesErrorPlot(scr_poly, full_test_labels, Cfn, Cfp, "Polynomial SVM")
+    _, w, b = score_calibration(5, SRad, labels_tra_rbm, 0.00001, testing=True)
+
+    ## Test Radial SVM
+    print("Testing RSVM...")
+    zi = 2 * labels_tra_rbm - 1
+    # scr_poly = calculate_svm(x, zi, attribute_tra_rbm, full_test_att, {"model":"radial", "gamma":gamma, "epsilon":0.1})
+    scr_poly=[]
+    for j in range(full_test_att.shape[1]):
+        score = 0
+        for i in range(attribute_tra_rbm.shape[1]):
+            tmp=(x[i]*zi[i]*(math.exp(-gamma*np.dot((full_test_att[:,j]-attribute_tra_rbm[:,i]).T,full_test_att[:,j]-attribute_tra_rbm[:,i]))+1**2))
+            score+=tmp
+        scr_poly.append(score)
+    scr_poly = np.array(scr_poly)
+    scr_poly_cal = np.dot(w.T, ML.vrow(scr_poly)) + b
+
+    for p in pi:
+        (minDCF, _, _, _) = ML.minCostBayes(scr_poly_cal, full_test_labels, p, Cfn, Cfp)
+        minDCffull_list.append(minDCF)
+    print("Radial SVM MinDCF: ", minDCffull_list[-2:], "Mean: ", np.mean(minDCffull_list[-2:]))
+    ML.BayesErrorPlot(scr_poly, full_test_labels, Cfn, Cfp, "Radial SVM")
+    ML.BayesErrorPlot(scr_poly_cal, full_test_labels, Cfn, Cfp, "Radial SVM Calibrated")
+
+    # # Calculate Polynomial SVM
+    # print("Training PSVM...")
+    # [Spoly,  minDCF, attribute_tra_poly, labels_tra_poly, x] = ML.k_fold(
+    #     k,
+    #     z_data,
+    #     full_train_label,
+    #     priorProb,
+    #     "polynomial",
+    #     pi=[0.1, 0.5],
+    #     C=C,
+    #     dim=dim,
+    #     c=c,
+    #     final=1,
+    # )
+    # _, w, b = score_calibration(5, Spoly, labels_tra_poly, 0.00001, testing=True)
+
+    # ## Test Polynomial SVM
+    # print("Testing PSVM...")
+    # zi = 2 * labels_tra_poly - 1
+    # scr_poly = calculate_svm(x, zi, attribute_tra_poly, z_test, {"model":"polynomial", "dim":dim, "c":c, "epsilon":0.1})
+    # scr_poly_cal = np.dot(w.T, ML.vrow(scr_poly)) + b
+    # for p in pi:
+    #     (minDCF, _, _, _) = ML.minCostBayes(scr_poly_cal, full_test_labels, p, Cfn, Cfp)
+    #     minDCffull_list.append(minDCF)
+    # print("Polynomial SVM MinDCF: ", minDCffull_list[-2:], "Mean: ", np.mean(minDCffull_list[-2:]))
+    # ML.BayesErrorPlot(scr_poly_cal, full_test_labels, Cfn, Cfp, "Polynomial SVM")
 
     ## Calculate GMM
-    [_, _, minDCF, multi_mu, cov, w] = ML.k_fold(
+    print("Training GMM...")
+    [Sgmm, labels_gmm, minDCF, multi_mu, cov, w] = ML.k_fold(
         k,
         full_train_att,
         full_train_label,
@@ -240,11 +326,16 @@ if __name__ == "__main__":
         niter=niter,
         final=1,
     )
+    _, w, b = score_calibration(5, Sgmm, labels_gmm, 0.00001, testing=True)
+    print("GMM MinDCF: ", minDCF)
 
     ## Test GMM
+    print("Testing GMM...")
     scr_gmm = calculate_gmm(full_test_att, full_test_labels, multi_mu, cov, w)
+    scr_gmm_cal = np.dot(w.T, scr_gmm) + b
     for p in pi:
-        (minDCF, _, _, _) = ML.minCostBayes(scr_gmm, full_test_labels, p, Cfn, Cfp)
+        (minDCF, _, _, _) = ML.minCostBayes(scr_gmm_cal, full_test_labels, p, Cfn, Cfp)
         minDCffull_list.append(minDCF)
     print("GMM MinDCF: ", minDCffull_list[-2:], "Mean: ", np.mean(minDCffull_list[-2:]))
     ML.BayesErrorPlot(scr_gmm, full_test_labels, Cfn, Cfp, "GMM")
+    ML.BayesErrorPlot(scr_gmm_cal, full_test_labels, Cfn, Cfp, "GMM")
